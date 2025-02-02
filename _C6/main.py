5d90627180314681b1a046367909086304e85e84
Annguyen <nguyenvanan01102003@gmail.com>
2025-02-02 13:55:57 +0700

    add filter_even_numbers function to return even integers from a list

---
+++ _C6/main.py
@@ -0,0 +1,13 @@
+"""
+This function takes a list of integers and returns a new list containing only the even numbers from the original list.
+
+Parameters:
+    numbers (list of int): A list of integers.
+
+Returns:
+    list of int: A list containing only the even integers from the input list.
+
+Example:
+    >>> filter_even_numbers([1, 2, 3, 4, 5, 6])
+    [2, 4, 6]
+"""
