0d9212bb3bf7ad93822a540da3b540ece0154bc7
Annguyen <nguyenvanan01102003@gmail.com>
2025-02-02 16:57:07 +0700

    add logic to write and read numbers from data.txt, sorting and printing the results

--- _C7/Bai42/main.py
+++ _C7/Bai42/main.py
@@ -1,1 +1,16 @@
 # Đây là file main.py cho Bai42
+numsB = []
+numsA = []
+
+for i in range(5):
+    numsB.append(int(i))
+
+with open("data.txt", "w") as f:
+    for num in numsB:
+        f.write(str(num) + "\n")
+
+with open("data.txt", "r") as f:
+    for line in f:
+        numsA.append(int(line))
+numsA.sort()
+print(numsA)
