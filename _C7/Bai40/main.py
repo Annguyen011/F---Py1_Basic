a29a4a3074b2595c1245cb509ca2135bf337e682
Annguyen <nguyenvanan01102003@gmail.com>
2025-02-02 16:28:28 +0700

    add PrintList function and main logic to display and modify a list of colors in main.py

--- _C7/Bai40/main.py
+++ _C7/Bai40/main.py
@@ -1,1 +1,16 @@
 # Đây là file main.py cho Bai40
+def PrintList(arr):
+    for i in range(len(arr)):
+        if i == len(arr) - 1:
+            print(arr[-1], end='.')
+            break
+        print(arr[i], end=', ')
+    print("")
+        
+def main():
+    colors = ['red', 'green', 'blue', 'yellow', 'white', 'black']
+    PrintList(colors)
+    colors.append('pink')
+    PrintList(colors)
+    
+main()
