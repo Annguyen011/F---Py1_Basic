e32958c649da5d11c3eed68a16de630636f1589e
Annguyen <nguyenvanan01102003@gmail.com>
2025-02-02 11:09:15 +0700

    add input validation to printNumber function and implement tinhTong for summation

--- Bai22/main.py
+++ Bai22/main.py
@@ -1,5 +1,20 @@
 def printNumber(a, b):
+    if type(a) != int or type(b) != int:
+        print("Invalid input")
+    
+    if type(int(a)) == int and type(int(b)) == int:
+        a = int(a)
+        b = int(b)
+    else: 
+        return 
+
     for i in range(a, b + 1):
         print(i, end = " ")
 
-printNumber(1, 10)
+def tinhTong(a, b):
+    sum = 0
+    for i in range(a, b + 1):
+        sum += i
+    return sum
+
+printNumber("1", 10)
