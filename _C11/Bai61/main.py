12b26fcf06e061c99a3ad138ee7320729663aa0f
Annguyen <nguyenvanan01102003@gmail.com>
2025-02-09 10:42:11 +0700

    Implement basic Pygame setup with a window and event handling in main.py for Bai61

--- _C11/Bai61/main.py
+++ _C11/Bai61/main.py
@@ -1,1 +1,16 @@
-# Đây là file main.py cho e:\Ai\DungLai-CoBan\_C11\Bai61
+import pygame
+
+pygame.init()
+running = True
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Hello World")
+while running:
+    pygame.time.Clock().tick(120)
+    screen.fill((255, 255, 255))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill((255, 255, 255))
+    pygame.display.flip()
+    
+pygame.quit()
