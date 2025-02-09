14e4aa61e0ea73fc70951346d14a9505f04bab11
Annguyen <nguyenvanan01102003@gmail.com>
2025-02-09 15:03:26 +0700

    Implement Pygame setup with mouse interaction and event handling in main.py for Bai64

--- _C11/Bai64/main.py
+++ _C11/Bai64/main.py
@@ -1,1 +1,37 @@
-# Đây là file main.py cho e:\Ai\DungLai-CoBan\_C11\Bai64
+import pygame
+
+pygame.init()
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+clock = pygame.time.Clock() 
+running = True
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Bird")
+
+while running:
+    clock.tick(120)
+    # screen.fill(GREEN)
+    mousePos = pygame.mouse.get_pos()
+    pygame.draw.rect(screen, RED, (100, 100, 100, 100))
+    
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button != 1:
+                break
+            if mousePos[0] > 200:
+                break
+            if mousePos[0] < 100:
+                break
+            if mousePos[1] > 200:
+                break
+            if mousePos[1] < 100:
+                break
+            screen.fill(BLUE)
+            
+        if event.type == pygame.QUIT:
+            running = False
+            
+    pygame.display.flip()
+
+pygame.quit()
