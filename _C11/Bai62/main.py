018805ae9d9bc242b0d610fdee6211715b3918bf
Annguyen <nguyenvanan01102003@gmail.com>
2025-02-09 10:58:25 +0700

    Implement Pygame setup with a basic window and event loop in main.py for Bai62

--- _C11/Bai62/main.py
+++ _C11/Bai62/main.py
@@ -1,1 +1,23 @@
-# Đây là file main.py cho e:\Ai\DungLai-CoBan\_C11\Bai62
+import pygame
+
+pygame.init()
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+clock = pygame.time.Clock() 
+running = True
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Bird")
+
+while running:
+    clock.tick(120)
+    screen.fill(GREEN)
+    
+    pygame.draw.rect(screen, RED, (100, 100, 100, 100))
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            
+    pygame.display.flip()
+
+pygame.quit()
