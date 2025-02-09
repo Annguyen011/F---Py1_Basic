09dc914875ea1f708fb29bbee3d1d1ba6ac34b86
Annguyen <nguyenvanan01102003@gmail.com>
2025-02-09 14:54:26 +0700

    Add mouse event handling to main.py in Bai62

--- _C11/Bai62/main.py
+++ _C11/Bai62/main.py
@@ -11,10 +11,13 @@ pygame.display.set_caption("Bird")
 while running:
     clock.tick(120)
     screen.fill(GREEN)
-    
+    mousePos = pygame.mouse.get_pos()
     pygame.draw.rect(screen, RED, (100, 100, 100, 100))
     
     for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print("Mouse Down")
+        
         if event.type == pygame.QUIT:
             running = False
             
