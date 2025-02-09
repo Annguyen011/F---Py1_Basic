3d95ac10a90e5c2aac8659c8b2377a31a222651e
Annguyen <nguyenvanan01102003@gmail.com>
2025-02-09 15:12:36 +0700

    Implement Pygame application with random background color change on mouse click in main.py for Bai65

--- _C11/Bai65/main.py
+++ _C11/Bai65/main.py
@@ -1,1 +1,43 @@
-# Đây là file main.py cho e:\Ai\DungLai-CoBan\_C11\Bai65
+import pygame
+import random
+
+
+pygame.init()
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+background = BLUE
+clock = pygame.time.Clock() 
+running = True
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Bird")
+
+font = pygame.font.SysFont('sans', 30)
+text = font.render('Random', True, (0, 0, 0))
+
+while running:
+    clock.tick(120)
+    screen.fill(background)
+    mousePos = pygame.mouse.get_pos()
+    pygame.draw.rect(screen, RED, (100, 100, 100, 100))
+    screen.blit(text, (100, 100))
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button != 1:
+                break
+            if mousePos[0] > 200:
+                break
+            if mousePos[0] < 100:
+                break
+            if mousePos[1] > 200:
+                break
+            if mousePos[1] < 100:
+                break
+            background = random.randint(0, 225), random.randint(0, 225), random.randint(0, 225)
+            
+        if event.type == pygame.QUIT:
+            running = False
+            
+    pygame.display.flip()
+
+pygame.quit()
